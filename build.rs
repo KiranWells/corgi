02861ba610163b58078a8d3a05935f3d9943500e
KiranWells <kiranwells1008@gmail.com>
2025-08-11 12:22:18 -0500

    Fix build cache issues

--- build.rs
+++ build.rs
@@ -1,4 +1,5 @@
 fn main() {
+    println!("cargo::rerun-if-changed=build.rs");
     wesl::Wesl::new("src/shaders").build_artifact(&"package::color".parse().unwrap(), "color");
     wesl::Wesl::new("src/shaders")
         .build_artifact(&"package::calculate".parse().unwrap(), "calculate");
