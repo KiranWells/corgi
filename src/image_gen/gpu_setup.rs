KiranWells/corgi

commit 61b177549d398a6838efa17bd54dfad39dcb8295

Add support for exporting all data in EXR

src/image_gen/gpu_setup.rs
@@ -12,8 +12,8 @@ use std::sync::{Arc, mpsc};
 
 use parking_lot::RwLock;
 use wgpu::{
-    self, BindGroup, BindGroupLayoutEntry, Buffer, ComputePipeline, Device, ExperimentalFeatures,
-    PipelineLayout, Queue, ShaderModule, Texture, TextureView,
+    self, BindGroup, BindGroupLayoutEntry, Buffer, BufferAsyncError, ComputePipeline, Device,
+    ExperimentalFeatures, PipelineLayout, Queue, ShaderModule, Texture, TextureView,
 };
 
 use crate::image_gen::shader_types::{
@@ -98,6 +98,14 @@ enum BuffType {
     HostReadable,
     /// A uniform buffer that can be written by the host.
     Uniform,
+}
+
+pub enum BufferId {
+    Step,
+    Orbit,
+    Stripe,
+    Z,
+    Dz,
 }
 
 #[derive(Clone, Copy, Debug)]
@@ -326,10 +334,10 @@ impl GPUData {
     }
 
     /// Load the data from the currently rendered image from the GPU to the CPU.
-    pub fn get_texture_data(&self) -> Option<Vec<u8>> {
+    pub fn get_texture_data(&self) -> Result<Vec<u8>, BufferAsyncError> {
         let (send, recv) = mpsc::channel();
         let ext = self.texture.read().size();
-        let padded_width = ((ext.width * 4) as f32 / 256.0).ceil() as usize * 256;
+        let padded_width = (ext.width * 4).div_ceil(256) as usize * 256;
         let tmp_buffer = Buffers::create_buffer::<u8>(
             &self.shared.device,
             padded_width * ext.height as usize,
@@ -360,21 +368,46 @@ impl GPUData {
             timeout: None,
         });
         match recv.recv() {
-            Ok(Ok(())) => {
-                let mut out = Vec::new();
-                for chunk in tmp_buffer.slice(..).get_mapped_range().chunks(padded_width) {
-                    out.extend_from_slice(&chunk[..(ext.width * 4) as usize]);
-                }
-                Some(out)
-            }
-            Ok(Err(err)) => {
-                tracing::error!("Error: {err:?}");
-                None
-            }
-            Err(err) => {
-                tracing::error!("Error: {err:?}");
-                None
-            }
+            Ok(Ok(())) => Ok(tmp_buffer.get_mapped_range(..).to_vec()),
+            Ok(Err(err)) => Err(err),
+            // we always send exactly one message
+            Err(_recv_err) => unreachable!(),
+        }
+    }
+
+    /// Load the data from the currently rendered image from the GPU to the CPU.
+    pub fn get_buffer_data(&self, id: BufferId) -> Result<Vec<u8>, BufferAsyncError> {
+        let (send, recv) = mpsc::channel();
+        let buffer = match id {
+            BufferId::Step => &self.buffers.step,
+            BufferId::Orbit => &self.buffers.orbits,
+            BufferId::Stripe => &self.buffers.stripes,
+            BufferId::Z => &self.buffers.delta_n,
+            BufferId::Dz => &self.buffers.delta_prime,
+        };
+        let tmp_buffer = Buffers::create_buffer::<u8>(
+            &self.shared.device,
+            buffer.size() as usize,
+            BuffType::HostReadable,
+        );
+        let mut encoder = self
+            .shared
+            .device
+            .create_command_encoder(&wgpu::CommandEncoderDescriptor { label: None });
+        encoder.copy_buffer_to_buffer(buffer, 0, &tmp_buffer, 0, None);
+        encoder.map_buffer_on_submit(&tmp_buffer, wgpu::MapMode::Read, .., move |res| {
+            let _ = send.send(res);
+        });
+        let si = self.shared.queue.submit([encoder.finish()]);
+        let _ = self.shared.device.poll(wgpu::PollType::Wait {
+            submission_index: Some(si),
+            timeout: None,
+        });
+        match recv.recv() {
+            Ok(Ok(())) => Ok(tmp_buffer.get_mapped_range(..).to_vec()),
+            Ok(Err(err)) => Err(err),
+            // we always send exactly one message
+            Err(_recv_err) => unreachable!(),
         }
     }
 }
@@ -406,7 +439,7 @@ impl Buffers {
             label: None,
             size: (size * core::mem::size_of::<T>()) as u64,
             usage: match ty {
-                ShaderOnly => wgpu::BufferUsages::STORAGE,
+                ShaderOnly => wgpu::BufferUsages::STORAGE | wgpu::BufferUsages::COPY_SRC,
                 HostWritable => wgpu::BufferUsages::STORAGE | wgpu::BufferUsages::COPY_DST,
                 HostReadable => wgpu::BufferUsages::MAP_READ | wgpu::BufferUsages::COPY_DST,
                 Uniform => wgpu::BufferUsages::UNIFORM | wgpu::BufferUsages::COPY_DST,

Cargo.toml
@@ -28,8 +28,10 @@ required-features = ["binary-deps"]
 
 [dependencies]
 bytemuck = { version = "1.23", features = ["derive"] }
+documented = "0.9.2"
 ecolor = { version = "0.33.0", features = ["serde", "bytemuck"] }
 emath = "0.33.3"
+exr = "1.74.0"
 image = { version = "0.25", features = ["avif-native"] }
 little_exif = "0.6.23"
 parking_lot = "0.12.5"
@@ -52,7 +54,6 @@ egui_taffy = { version = "0.10.0", optional = true }
 rfd = { version = "0.15.4", features = ["xdg-portal"], optional = true }
 taffy = { version = "0.7.7", optional = true }
 tracing-subscriber = { version = "0.3", optional = true, features = ["env-filter"] }
-documented = "0.9.2"
 egui_extras = { version = "0.33.3", optional = true, features = ["file", "image"] }
 nanoid = { version = "0.5.0", optional = true }
 rand = { version = "0.10.1", optional = true }
@@ -67,3 +68,5 @@ wesl = "0.3.2"
 [profile.dev.package."*"]
 opt-level = 3
 
+[profile.release]
+lto = true

src/image_gen/mod.rs
@@ -48,8 +48,12 @@ pub enum RenderingError {
 pub enum SaveError {
     #[error("There is no rendered image")]
     NoImage,
+    #[error("Failed to load data from GPU")]
+    FailedLoad(#[from] wgpu::BufferAsyncError),
     #[error("Failed to save the image data to a file")]
     Save(#[from] image::ImageError),
+    #[error("Failed to save the image data to an EXR file")]
+    ExrSave(#[from] exr::error::Error),
     #[error("Failed to serialize image spec")]
     Serialization(#[from] crate::types::serde::SaveLoadError),
     #[error("Metadata not supported for image format: {0:?}")]
@@ -347,7 +351,7 @@ impl Engine {
             return Err(SaveError::NoImage);
         }
         status_callback(ProgressUpdate::msg("Fetching image data"));
-        if let Some(data) = self.gpu_data.get_texture_data() {
+        if let Ok(data) = self.gpu_data.get_texture_data() {
             status_callback(ProgressUpdate::msg("Saving image"));
             let mut img = image::DynamicImage::ImageRgba8(
                 ImageBuffer::from_raw(image_settings.width, image_settings.height, data)
@@ -448,6 +452,199 @@ impl Engine {
         } else {
             None
         }
+    }
+
+    pub fn save_to_exr(
+        &self,
+        path: &Path,
+        status_callback: &mut impl FnMut(ProgressUpdate),
+    ) -> Result<(), SaveError> {
+        use exr::prelude::*;
+        let size = self.gpu_data.texture.read().size();
+        let size = (size.width as usize, size.height as usize);
+        // Color layer
+        let color_data = self.gpu_data.get_texture_data()?;
+
+        fn byte_to_linear_f16(byte: u8) -> f16 {
+            f16::from_f32((((byte as f32) / 256.0 + 0.055) / 1.055).powf(2.4))
+        }
+
+        let color_channels = SpecificChannels::rgb(|loc: Vec2<usize>| {
+            (
+                byte_to_linear_f16(color_data[(loc.x() + loc.y() * size.0) * 4]),
+                byte_to_linear_f16(color_data[(loc.x() + loc.y() * size.0) * 4 + 1]),
+                byte_to_linear_f16(color_data[(loc.x() + loc.y() * size.0) * 4 + 2]),
+            )
+        });
+
+        let color_layer = Layer::new(
+            size,
+            LayerAttributes {
+                layer_name: Some("Final Color".into()),
+                ..Default::default()
+            },
+            Encoding {
+                compression: Compression::B44A,
+                ..Default::default()
+            },
+            color_channels,
+        );
+
+        // Step layer
+        let step_data = self.gpu_data.get_buffer_data(gpu_setup::BufferId::Step)?;
+
+        let step_channels = SpecificChannels::build()
+            .with_channel("x") // step
+            .with_channel("y") // escaped
+            .with_pixel_fn(|loc: Vec2<usize>| {
+                let index = (loc.x() + loc.y() * size.0) * 4;
+                let steps = i32::from_ne_bytes(step_data[index..index + 4].try_into().unwrap());
+                (steps.unsigned_abs(), ((steps.signum() + 1) / 2) as u32)
+            });
+
+        let step_layer = Layer::new(
+            size,
+            LayerAttributes {
+                layer_name: Some("Steps".into()),
+                ..Default::default()
+            },
+            Encoding {
+                compression: Compression::PXR24,
+                ..Default::default()
+            },
+            step_channels,
+        );
+
+        // Z layer
+        let z_data = self.gpu_data.get_buffer_data(gpu_setup::BufferId::Z)?;
+
+        let z_channels = SpecificChannels::build()
+            .with_channel("x") // real
+            .with_channel("y") // imaginary
+            .with_channel("z") // scale
+            .with_pixel_fn(|loc: Vec2<usize>| {
+                let index = (loc.x() + loc.y() * size.0) * 16;
+                let x = f32::from_ne_bytes(z_data[index..index + 4].try_into().unwrap());
+                let y = f32::from_ne_bytes(z_data[index + 4..index + 8].try_into().unwrap());
+                let scale = f32::from_ne_bytes(z_data[index + 8..index + 12].try_into().unwrap());
+                (x, y, scale)
+            });
+
+        let z_layer = Layer::new(
+            size,
+            LayerAttributes {
+                layer_name: Some("Z".into()),
+                ..Default::default()
+            },
+            Encoding {
+                compression: Compression::PIZ,
+                ..Default::default()
+            },
+            z_channels,
+        );
+
+        // dz layer
+        let dz_data = self.gpu_data.get_buffer_data(gpu_setup::BufferId::Dz)?;
+
+        let dz_channels = SpecificChannels::build()
+            .with_channel("x") // real
+            .with_channel("y") // imaginary
+            .with_channel("z") // scale
+            .with_pixel_fn(|loc: Vec2<usize>| {
+                let index = (loc.x() + loc.y() * size.0) * 16;
+                let x = f32::from_ne_bytes(dz_data[index..index + 4].try_into().unwrap());
+                let y = f32::from_ne_bytes(dz_data[index + 4..index + 8].try_into().unwrap());
+                let scale = f32::from_ne_bytes(dz_data[index + 8..index + 12].try_into().unwrap());
+                (x, y, scale)
+            });
+
+        let dz_layer = Layer::new(
+            size,
+            LayerAttributes {
+                layer_name: Some("dZ".into()),
+                ..Default::default()
+            },
+            Encoding {
+                compression: Compression::PIZ,
+                ..Default::default()
+            },
+            dz_channels,
+        );
+
+        // orbit layer
+        let orbit_data = self.gpu_data.get_buffer_data(gpu_setup::BufferId::Orbit)?;
+
+        let orbit_channels = SpecificChannels::build()
+            .with_channel("x")
+            .with_channel("y")
+            .with_channel("z")
+            .with_channel("w")
+            .with_pixel_fn(|loc: Vec2<usize>| {
+                let index = (loc.x() + loc.y() * size.0) * 16;
+                let x = f32::from_ne_bytes(orbit_data[index..index + 4].try_into().unwrap());
+                let y = f32::from_ne_bytes(orbit_data[index + 4..index + 8].try_into().unwrap());
+                let z = f32::from_ne_bytes(orbit_data[index + 8..index + 12].try_into().unwrap());
+                let w = f32::from_ne_bytes(orbit_data[index + 12..index + 16].try_into().unwrap());
+                (x, y, z, w)
+            });
+
+        let orbit_layer = Layer::new(
+            size,
+            LayerAttributes {
+                layer_name: Some("Orbit".into()),
+                ..Default::default()
+            },
+            Encoding {
+                compression: Compression::PIZ,
+                ..Default::default()
+            },
+            orbit_channels,
+        );
+
+        // stripe layer
+        let stripe_data = self.gpu_data.get_buffer_data(gpu_setup::BufferId::Stripe)?;
+
+        let stripe_channels = SpecificChannels::build()
+            .with_channel("x")
+            .with_channel("y")
+            .with_channel("z")
+            .with_channel("w")
+            .with_pixel_fn(|loc: Vec2<usize>| {
+                let index = (loc.x() + loc.y() * size.0) * 16;
+                let x = f32::from_ne_bytes(stripe_data[index..index + 4].try_into().unwrap());
+                let y = f32::from_ne_bytes(stripe_data[index + 4..index + 8].try_into().unwrap());
+                let z = f32::from_ne_bytes(stripe_data[index + 8..index + 12].try_into().unwrap());
+                let w = f32::from_ne_bytes(stripe_data[index + 12..index + 16].try_into().unwrap());
+                (x, y, z, w)
+            });
+
+        let stripe_layer = Layer::new(
+            size,
+            LayerAttributes {
+                layer_name: Some("Stripe".into()),
+                ..Default::default()
+            },
+            Encoding {
+                compression: Compression::PIZ,
+                ..Default::default()
+            },
+            stripe_channels,
+        );
+
+        Image::empty(ImageAttributes::with_size(size))
+            .with_layer(color_layer)
+            .with_layer(step_layer)
+            .with_layer(z_layer)
+            .with_layer(dz_layer)
+            .with_layer(orbit_layer)
+            .with_layer(stripe_layer)
+            .write()
+            .on_progress(|progress| {
+                status_callback(ProgressUpdate::partial("Writing EXR file", progress))
+            })
+            .to_file(path)?;
+
+        Ok(())
     }
 }
 

src/main.rs
@@ -119,13 +119,17 @@ fn headless_render(
     };
     let timings = engine.render_image(&image, &mut status_callback)?;
     print!("\n------- | Rendering timings: {}", timings);
-    engine.save_to_file(
-        path,
-        None,
-        compression_params,
-        corgi_lib::types::serde::is_metadata_supported(path),
-        &mut status_callback,
-    )?;
+    if path.extension() == Some(&std::ffi::OsString::from("exr")) {
+        engine.save_to_exr(path, &mut status_callback)?;
+    } else {
+        engine.save_to_file(
+            path,
+            None,
+            compression_params,
+            corgi_lib::types::serde::is_metadata_supported(path),
+            &mut status_callback,
+        )?;
+    }
     println!();
     Ok(())
 }

src/types/image.rs
@@ -8,6 +8,12 @@ use super::Coloring;
 use crate::image_gen::shader_types::Transform;
 use crate::types::{LayerKind, Rotate};
 
+pub const STRIPES_ENABLED: u32 = 0x1;
+pub const TOTAL_ANGLE_ENABLED: u32 = 0x2;
+pub const ORBIT_ENABLED: u32 = 0x4;
+pub const DERIVATIVE_ENABLED: u32 = 0x8;
+pub const JULIA: u32 = 0x1000_0000;
+
 /// A representation of the current fractal being rendered, including
 /// the fractal location, settings, coloring, and image parameters
 #[derive(Debug, Clone, PartialEq)]
@@ -237,11 +243,6 @@ impl ImgSpec {
     /// Returns the set of bit flags to send to the shader based on the
     /// features necessary to render this image and the optimization level.
     pub fn get_flags(&self) -> u32 {
-        const STRIPES_ENABLED: u32 = 0x1;
-        const TOTAL_ANGLE_ENABLED: u32 = 0x2;
-        const ORBIT_ENABLED: u32 = 0x4;
-        const DERIVATIVE_ENABLED: u32 = 0x8;
-        const JULIA: u32 = 0x1000_0000;
         let kind_flags = match &self.location.fractal_kind {
             FractalKind::Mandelbrot => 0,
             FractalKind::Julia(_) => JULIA,

src/ui/mod.rs
@@ -78,8 +78,17 @@ struct StyleTabState {
 }
 #[derive(Debug)]
 struct RenderTabState {
+    exr_mode: bool,
+    state: RenderState,
     rendered_view: View,
     save_path: PathBuf,
+}
+#[derive(Debug, PartialEq)]
+enum RenderState {
+    Init,
+    Rendering,
+    Cancelled,
+    Rendered,
 }
 
 /// The main UI state struct.
@@ -95,7 +104,6 @@ pub struct CorgiUI {
     setting_probe: bool,
     show_camera: bool,
     show_settings: bool,
-    rendering_output: bool,
     /// Whether the viewport should load the current image from
     /// the rendering thread into the UI view.
     pub swap: bool,
@@ -145,6 +153,8 @@ impl CorgiUI {
                 ),
             },
             render_state: RenderTabState {
+                exr_mode: false,
+                state: RenderState::Init,
                 rendered_view: image.view(),
                 save_path: context.cache().previous_paths.image.clone(),
             },
@@ -161,7 +171,6 @@ impl CorgiUI {
             preset_name: "New Preset".into(),
             preset_group: "New Group".into(),
             new_group_active: false,
-            rendering_output: false,
             status: Status::default(),
             command_channel,
         }
@@ -572,6 +581,41 @@ impl CorgiUI {
         cancel: impl FnOnce(),
         tui: &mut egui_taffy::Tui,
     ) {
+        tui.style(Style::row()).add(|tui| {
+            tui.egui_style_mut().visuals.widgets.inactive.corner_radius = CornerRadius::ZERO;
+            tui.egui_style_mut().visuals.widgets.active.corner_radius = CornerRadius::ZERO;
+            tui.egui_style_mut().visuals.widgets.hovered.corner_radius = CornerRadius::ZERO;
+            if tui
+                .style(Style::grow().center())
+                .selectable(!self.render_state.exr_mode, |tui| {
+                    let fill = if !self.render_state.exr_mode {
+                        tui.egui_ui().style().visuals.window_fill
+                    } else {
+                        tui.egui_ui().visuals().text_color()
+                    };
+                    tui.label(egui::RichText::new("Save Image").heading().color(fill));
+                })
+                .clicked()
+            {
+                self.render_state.exr_mode = false;
+                self.render_state.state = RenderState::Init;
+            }
+            if tui
+                .style(Style::grow().center())
+                .selectable(self.render_state.exr_mode, |tui| {
+                    let fill = if self.render_state.exr_mode {
+                        tui.egui_ui().style().visuals.window_fill
+                    } else {
+                        tui.egui_ui().visuals().text_color()
+                    };
+                    tui.label(egui::RichText::new("Export Data").heading().color(fill));
+                })
+                .clicked()
+            {
+                self.render_state.exr_mode = true;
+                self.render_state.state = RenderState::Init;
+            }
+        });
         section(tui, "Image Settings", true, |tui| {
             input_with_label(
                 tui,
@@ -636,10 +680,14 @@ impl CorgiUI {
                     self.render_state.save_path = path;
                 }
             });
-            let mut compression_params = context.cache().compression_params;
-            compression_params.render_edit_ui(ctx, tui);
-            if context.cache().compression_params != compression_params {
-                context.cache_mut().compression_params = compression_params;
+            if self.render_state.exr_mode {
+                // TODO: add layer selection
+            } else {
+                let mut compression_params = context.cache().compression_params;
+                compression_params.render_edit_ui(ctx, tui);
+                if context.cache().compression_params != compression_params {
+                    context.cache_mut().compression_params = compression_params;
+                }
             }
         });
         let item_spacing = tui.egui_ui().spacing().item_spacing;
@@ -649,38 +697,62 @@ impl CorgiUI {
                 .gap(item_spacing.y * 2.0),
         )
         .add(|tui| {
-            if !self.rendering_output {
+            if self.render_state.state != RenderState::Rendering {
                 if tui.ui_add(Button::new("Render")).clicked() {
-                    let image = self.root_spec.clone();
+                    let mut image = self.root_spec.clone();
+                    if self.render_state.exr_mode {
+                        image.optimization_level = OptLevel::CacheOptimized;
+                    }
                     let _ = self
                         .command_channel
                         .send(ImageGenCommand::Render(RendererId::Render, Box::new(image)));
-                    self.rendering_output = true;
+                    self.render_state.state = RenderState::Rendering;
                 }
             } else if tui.ui_add(Button::new("Cancel Render")).clicked() {
-                self.rendering_output = false;
+                self.render_state.state = RenderState::Cancelled;
                 cancel();
             }
-            if tui.ui_add(Button::new("Save to file")).clicked()
-                && let Some(path) = rfd::FileDialog::new()
-                    .set_directory(&self.render_state.save_path)
-                    .add_filter(
-                        "image with metadata",
-                        &["avif", "jpg", "jpeg", "webp", "png"],
-                    )
-                    .add_filter("image without metadata", &["gif", "qoi", "tiff", "exr"])
-                    .set_file_name(format!("fractal.{}", context.cache().default_image_type))
-                    .save_file()
+            if tui
+                .enabled_ui(self.render_state.state == RenderState::Rendered)
+                .ui_add(Button::new(if self.render_state.exr_mode {
+                    "Save as EXR"
+                } else {
+                    "Save Image"
+                }))
+                .clicked()
             {
-                if let Some(ext) = path.extension().and_then(OsStr::to_str) {
-                    context.cache_mut().default_image_type = ext.to_owned();
+                let mut dialog = rfd::FileDialog::new().set_directory(&self.render_state.save_path);
+                if self.render_state.exr_mode {
+                    dialog = dialog.add_filter("EXR", &["exr"]);
+                } else {
+                    dialog = dialog
+                        .add_filter(
+                            "image with metadata",
+                            &["avif", "jpg", "jpeg", "webp", "png"],
+                        )
+                        .add_filter("image without metadata", &["gif", "qoi", "tiff"])
                 }
-                let _ = self.command_channel.send(ImageGenCommand::SaveToFile(
-                    RendererId::Render,
-                    path.clone(),
-                    context.cache().compression_params,
-                    None,
-                ));
+                let extension = if self.render_state.exr_mode {
+                    "exr"
+                } else {
+                    &context.cache().default_image_type
+                };
+                if let Some(path) = dialog
+                    .set_file_name(format!("fractal.{}", extension))
+                    .save_file()
+                {
+                    if let Some(ext) = path.extension().and_then(OsStr::to_str)
+                        && !self.render_state.exr_mode
+                    {
+                        context.cache_mut().default_image_type = ext.to_owned();
+                    }
+                    let _ = self.command_channel.send(ImageGenCommand::SaveToFile(
+                        RendererId::Render,
+                        path.clone(),
+                        context.cache().compression_params,
+                        None,
+                    ));
+                }
             }
         });
     }
@@ -1219,7 +1291,7 @@ impl CorgiUI {
                 view.height = self.current_view.height;
                 self.current_view = view;
                 self.current_view.zoom_to_fit(&viewport);
-                self.rendering_output = false;
+                self.render_state.state = RenderState::Rendered;
             }
             RendererId::Thumbnail => {}
         }

src/worker.rs
@@ -223,16 +223,25 @@ impl WorkerState {
                 self.ctx.request_repaint();
             }
             for (id, (path, compression_params, name)) in save_commands {
-                if let Err(err) = self.renderers.get(&id).unwrap().save_to_file(
-                    &path,
-                    name,
-                    compression_params,
-                    corgi_lib::types::serde::is_metadata_supported(&path),
-                    &mut |pu| {
-                        let _ = self.status_channel.send(StatusMessage::Progress(pu));
-                        self.ctx.request_repaint();
-                    },
-                ) {
+                let mut status_callback = |pu| {
+                    let _ = self.status_channel.send(StatusMessage::Progress(pu));
+                    self.ctx.request_repaint();
+                };
+                let result = if path.extension() == Some(&std::ffi::OsString::from("exr")) {
+                    self.renderers
+                        .get_mut(&id)
+                        .unwrap()
+                        .save_to_exr(&path, &mut status_callback)
+                } else {
+                    self.renderers.get(&id).unwrap().save_to_file(
+                        &path,
+                        name,
+                        compression_params,
+                        corgi_lib::types::serde::is_metadata_supported(&path),
+                        &mut status_callback,
+                    )
+                };
+                if let Err(err) = result {
                     let _ = self.status_channel.send(StatusMessage::Error(err.into()));
                 } else {
                     let _ = self

commit fbfd22b49a5041defef79f1fe2d15a3d1299a789

Fix image export for non-padding aligned image size
This issue was introduced in 61b1775 when the padding adjust logic was
mistakenly removed.

src/image_gen/gpu_setup.rs
@@ -368,7 +368,13 @@ impl GPUData {
             timeout: None,
         });
         match recv.recv() {
-            Ok(Ok(())) => Ok(tmp_buffer.get_mapped_range(..).to_vec()),
+            Ok(Ok(())) => {
+                let mut out = Vec::new();
+                for chunk in tmp_buffer.slice(..).get_mapped_range().chunks(padded_width) {
+                    out.extend_from_slice(&chunk[..(ext.width * 4) as usize]);
+                }
+                Ok(out)
+            }
             Ok(Err(err)) => Err(err),
             // we always send exactly one message
             Err(_recv_err) => unreachable!(),